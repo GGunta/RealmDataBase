27a16b552c4a007023975e4ad1e1333544304c87
GGunta <msdoors@gmail.com>
2021-09-01 18:22:15 +0300

    Initial Commit

---
+++ RealmDataBase/RealmDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmDataBaseApp.swift
+//  RealmDataBase
+//
+//  Created by gunta.golde on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
